907754adec8ac0096a90f37977e454136a67f38c
/matheus <dmosken2015@gmail.com>
2023-06-03 21:07:03 -0300

    add files to commit

--- estudos/assembly/aula01/hello.asm
+++ estudos/assembly/aula01/hello.asm
@@ -1,22 +1,29 @@
+; nams -f elf64 -o hello.o hello.asm
+; transforma o programa em linguagem de máquina
+; linkedit -o hello hello.o
+; cria o executável
+; ./hello
+; executa o programa
+
+; nasm -f elf64 -o hello.o hello.asm && ld -o hello hello.o && ./hello
 section .data
+    msg db "Hello, World!", 0xA ; 0xA é o caractere de nova linha
+    
+    tam equ $ - msg ; $ é o endereço do último byte da seção
+    msgC db "Hello, World!", 0xA, 0x0 ; 0x0 é o caractere de fim de string
 
+section .text ; seção de código
 
-section .text
-
-global _start
+global _start ; ponto de entrada do programa
 
 _start:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, msg
-    mov edx, 13
-    int 80h
+    mov EAX, 0x4 ; é o código da syscall write
+    mov EBX, 0x1 ; é o descritor de arquivo stdout
+    mov ECX, msg ; é o endereço da mensagem
+    mov EDX, tam ; é o tamanho da mensagem
+    int 0x80 ; é a chamada do sistema
 
-    mov eax, 1
-    mov ebx, 0
-    int 80h
-
-section .data
-    msg db 'Hello, world!', 0xa
-```
+    mov EAX, 0x1 ; é o código da syscall exit
+    mov EBX, 0x0 ; é o código de saída
+    int 0x80 ; é a chamada do sistema
 
