85be8a438ff85d642b498e71ef0961e9212abe1e
/matheus <dmosken2015@gmail.com>
2023-06-15 17:19:05 -0300

    add hello world files

--- estudos/assembly/aula01/hello.asm
+++ estudos/assembly/aula01/hello.asm
@@ -1,29 +1,21 @@
-; nams -f elf64 -o hello.o hello.asm
-; transforma o programa em linguagem de máquina
-; linkedit -o hello hello.o
-; cria o executável
-; ./hello
-; executa o programa
-
-; nasm -f elf64 -o hello.o hello.asm && ld -o hello hello.o && ./hello
 section .data
-    msg db "Hello, World!", 0xA ; 0xA é o caractere de nova linha
-    
-    tam equ $ - msg ; $ é o endereço do último byte da seção
-    msgC db "Hello, World!", 0xA, 0x0 ; 0x0 é o caractere de fim de string
+    HelloWorld db "Hello, World!", 0
 
-section .text ; seção de código
-
-global _start ; ponto de entrada do programa
+section .text
+    global _start
 
 _start:
-    mov EAX, 0x4 ; é o código da syscall write
-    mov EBX, 0x1 ; é o descritor de arquivo stdout
-    mov ECX, msg ; é o endereço da mensagem
-    mov EDX, tam ; é o tamanho da mensagem
-    int 0x80 ; é a chamada do sistema
+    ; Escrever a string "Hello, World!" no console
+    mov rax, 1  ; Número da syscall para write
+    mov rdi, 1  ; Handle do arquivo de saída (STDOUT)
+    mov rsi, HelloWorld  ; Endereço da string
+    mov rdx, length_of_string  ; Comprimento da string
+    syscall
 
-    mov EAX, 0x1 ; é o código da syscall exit
-    mov EBX, 0x0 ; é o código de saída
-    int 0x80 ; é a chamada do sistema
+    ; Sair do programa
+    mov eax, 60  ; Número da syscall para exit
+    xor edi, edi  ; Código de saída 0
+    syscall
 
+section .data
+    length_of_string equ $ - HelloWorld
